b9a27c5c1e3a38ac443d8a92ce390cba34619741
klub-ice <114689677+klub-ice@users.noreply.github.com>
2026-09-09 19:52:51 -0500

    IsOdd

---
+++ isOdd.c
@@ -0,0 +1,14 @@
+//
+// Created by Asus on 9/9/2026.
+//
+
+#include "isOdd.h"
+#include  <stdio.h>
+
+int isOdd(int num)
+{
+    if (num % 2 != 0){
+        printf("%d is odd.\n", num);
+    }
+    return 0;
+}
---
+++ isOdd.h
@@ -0,0 +1,9 @@
+//
+// Created by Asus on 9/9/2026.
+//
+
+#ifndef ISODD_H
+#define ISODD_H
+int isOdd(int num);
+
+#endif //ISODD_H
